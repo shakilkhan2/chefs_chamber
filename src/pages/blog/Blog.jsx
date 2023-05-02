2c2e756cd4b1280ad3d26f65394f4d86fbd99bf3
shakilkhan2 <dev.shakilkhan@gmail.com>
2023-05-02 19:04:05 +0600

    blog added

--- src/pages/blog/Blog.jsx
+++ src/pages/blog/Blog.jsx
@@ -3,7 +3,28 @@ import React from "react";
 const Blog = () => {
   return (
     <div>
-      <h1>This is blog page.</h1>
+      <h1 className="text-4xl font-semibold text-amber-700 text-center mt-6 mb-2">Welcome to my blog</h1>
+      <hr className="w-[40%] mx-auto mb-6 border-amber-700" />
+      <div className="mx-32 mb-6">
+        <h2 className="text-2xl  text-amber-500"><span className="text-amber-700">1.</span> What is the differences between uncontrolled and controlled components?</h2>
+        <p><span className="text-xl text-amber-700">Ans:</span>
+Uncontrolled components are form inputs in React that maintain their own internal state and do not receive any updates from the parent component. Controlled components, on the other hand, are form inputs that receive their value from the parent component and notify the parent component of any changes. In short, controlled components are more closely managed by the parent component, while uncontrolled components manage their own state.</p>
+      </div>
+      <div className="mx-32 mb-6">
+        <h2 className="text-2xl  text-amber-500"><span className="text-amber-700">1.</span> What is the differences between uncontrolled and controlled components?</h2>
+        <p><span className="text-xl text-amber-700">Ans:</span>
+Uncontrolled components are form inputs in React that maintain their own internal state and do not receive any updates from the parent component. Controlled components, on the other hand, are form inputs that receive their value from the parent component and notify the parent component of any changes. In short, controlled components are more closely managed by the parent component, while uncontrolled components manage their own state.</p>
+      </div>
+      <div className="mx-32 mb-6">
+        <h2 className="text-2xl  text-amber-500"><span className="text-amber-700">1.</span> What is the differences between uncontrolled and controlled components?</h2>
+        <p><span className="text-xl text-amber-700">Ans:</span>
+Uncontrolled components are form inputs in React that maintain their own internal state and do not receive any updates from the parent component. Controlled components, on the other hand, are form inputs that receive their value from the parent component and notify the parent component of any changes. In short, controlled components are more closely managed by the parent component, while uncontrolled components manage their own state.</p>
+      </div>
+      <div className="mx-32 mb-6">
+        <h2 className="text-2xl  text-amber-500"><span className="text-amber-700">1.</span> What is the differences between uncontrolled and controlled components?</h2>
+        <p><span className="text-xl text-amber-700">Ans:</span>
+Uncontrolled components are form inputs in React that maintain their own internal state and do not receive any updates from the parent component. Controlled components, on the other hand, are form inputs that receive their value from the parent component and notify the parent component of any changes. In short, controlled components are more closely managed by the parent component, while uncontrolled components manage their own state.</p>
+      </div>
     </div>
   );
 };
--- src/pages/shared/footer/Footer.jsx
+++ src/pages/shared/footer/Footer.jsx
@@ -53,10 +53,12 @@ const Footer = () => {
               >
                 <path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path>
               </svg>
-            </a>
+            </a> 
+            
           </div>
+          <p className="text-center mt-12">Copyright © 2012 - 2023 Chefs'®. All rights reserved</p>
         </div>
-        <p className="text-center">Copyright © 2012 - 2023 Chefs'®. All rights reserved</p>
+       
       </footer>
     </div>
   );
