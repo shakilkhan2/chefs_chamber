40684066d453296eb180b533bda8fe628203af56
shakilkhan2 <dev.shakilkhan@gmail.com>
2023-05-02 21:43:45 +0600

    error page added

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -4,6 +4,7 @@ import Home from "../pages/home/Home";
 import Blog from "../pages/blog/Blog";
 import Login from "../pages/login/Login";
 import Register from "../pages/register/Register";
+import ErrorPage from "../pages/error/ErrorPage";
 
 const router = createBrowserRouter([
   {
@@ -13,20 +14,24 @@ const router = createBrowserRouter([
       {
         path: "/",
         element: <Home />,
-        loader: () => fetch(`http://localhost:5000/chefs`)
+        loader: () => fetch(`http://localhost:5000/chefs`),
       },
       {
         path: "/blog",
-        element: <Blog/>,
+        element: <Blog />,
       },
       {
         path: "/login",
-        element: <Login/>
+        element: <Login />,
       },
       {
         path: "/register",
-        element: <Register/>
-      }
+        element: <Register />,
+      },
+      {
+        path: "*",
+        element: <ErrorPage />,
+      },
     ],
   },
 ]);
